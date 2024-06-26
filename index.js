93c80b412d1ffad51d8fb612ab8176994c21b84c
Rebecca Jesson <rebeccajesson1@gmail.com>
2024-06-26 22:30:47 +0100

    Added support for more than two numbers

--- index.js
+++ index.js
@@ -3,35 +3,37 @@ const readline = require('readline-sync');
 // saying hello!
 console.log('Welcome to the calculator! :D');
 
-// getting input for operator and values
+// getting input for operator and number of iterations
 console.log('This calculator can perform operations using the four operators: +, -, *, /.\nPlease enter the operator you would like to use:');
 const operator = readline.prompt();
 
-console.log('\nPlease enter the first number:');
-const input1 = readline.prompt();
-const number1 = +input1
-
-console.log('\nPlease enter the second number:');
-const input2 = readline.prompt();
-const number2 = +input2
+console.log('How many numbers do you wish to ' + operator + ' ?');
+const iterationsStr = readline.prompt();
+const iterations = +iterationsStr
 
 let ans = 0;
 
-switch (operator){
-    case "+":
-        ans = number1 + number2;
-        break;
-    case "-":
-        ans = number1 - number2;
-        break;
-    case "*":
-        ans = number1 * number2;
-        break;
-    case "/":
-        ans = number1 / number2;
-        break;
-    default:
-        console.log('Your operator input was not one of the four options.')
+// request values and perform calculation using for loop
+for (let i = 0; i < iterations; i++) {
+    console.log('\nPlease enter number ' + (i + 1) + ':');
+    const input = readline.prompt();
+    const num = +input;
+
+    if (i == 0){
+        ans = num;
+    } else {
+        if (operator == '+'){
+            ans += num;
+        } else if (operator == '-'){
+            ans -= num;
+        } else if (operator == '*'){
+            ans *= num;
+        } else if (operator == '/'){
+            ans /= num;
+        } else {
+            console.log('Operator input invalid.')
+        }
+    }
 }
 
 console.log('\nThe answer is ' + ans);
