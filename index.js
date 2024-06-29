1ddaccb1a837cffdc7aff210f4ed3cde02347c5e
Rebecca Jesson <rebeccajesson1@gmail.com>
2024-06-30 00:59:00 +0100

    Added checks for number inputs, and calculator runs indefinitely

--- index.js
+++ index.js
@@ -1,40 +1,69 @@
 const readline = require('readline-sync');
 
-// saying hello!
-console.log('Welcome to the calculator! :D');
-
-// getting input for operator and number of iterations
-console.log('This calculator can perform operations using the four operators: +, -, *, /.\nPlease enter the operator you would like to use:');
-const operator = readline.prompt();
-
-console.log('How many numbers do you wish to ' + operator + ' ?');
-const iterationsStr = readline.prompt();
-const iterations = +iterationsStr
-
-let ans = 0;
-
-// request values and perform calculation using for loop
-for (let i = 0; i < iterations; i++) {
-    console.log('\nPlease enter number ' + (i + 1) + ':');
-    const input = readline.prompt();
-    const num = +input;
-
-    if (i == 0){
-        ans = num;
-    } else {
-        if (operator == '+'){
-            ans += num;
-        } else if (operator == '-'){
-            ans -= num;
-        } else if (operator == '*'){
-            ans *= num;
-        } else if (operator == '/'){
-            ans /= num;
-        } else {
-            console.log('Operator input invalid.')
-        }
-    }
+printWelcomeMessage();
+while (true) {
+    performOneCalculation();
 }
 
-console.log('\nThe answer is ' + ans);
+function printWelcomeMessage() {
+    console.log('Welcome to the calculator! :D');
+    console.log('This calculator can perform operations using the four operators: +, -, *, /.');
+}
 
+function getInput(prompt) {
+    console.log('\n' + prompt);
+    return readline.prompt();
+}
+
+function getNumberInput() {
+    let response;
+    do {
+        response = getInput('Please enter a number:');
+    } while (isNaN(response));
+    return +response;
+}
+
+function getOperatorChoice() {
+    return getInput('Please enter the operator you would like to use: ');
+}
+
+function getIterationValue() {
+    console.log('How many numbers do you wish to ' + operator + ' ?');
+    return getNumberInput() ;
+}
+
+function computeCalculation(operator, iterations) {
+    let ans = 0;
+
+    for (let i = 0; i < iterations; i++) {
+        console.log('Please enter number (' + (i+1) + ').')
+        num = getNumberInput();
+
+        if (i == 0){
+            ans = num;
+        } else {
+            if (operator == '+'){
+                ans += num;
+            } else if (operator == '-'){
+                ans -= num;
+            } else if (operator == '*'){
+                ans *= num;
+            } else if (operator == '/'){
+                ans /= num;
+            } else {
+                console.log('Operator input invalid.');
+            }
+        }
+    }
+
+    return ans;
+}
+
+function performOneCalculation() {
+    operator = getOperatorChoice();
+    iterations = getIterationValue();
+
+    ans = computeCalculation(operator, iterations);
+
+    console.log('\nThe answer is ' + ans + '.');
+}
