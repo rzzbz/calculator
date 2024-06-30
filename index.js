3086b171c8627d707236bbd7d3ab21e8cb01cd43
Rebecca Jesson <rebeccajesson1@gmail.com>
2024-06-30 18:26:28 +0100

    Added calculator mode for counting vowels in a string

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const readline = require('readline-sync');
+const ARITHMETIC_MODE = '1';
+const VOWEL_COUNTING_MODE = '2';
 
 printWelcomeMessage();
 while (true) {
-    performOneCalculation();
+    const calculationMode = getCalculationMode();
+    if (calculationMode === ARITHMETIC_MODE) {
+        performOneArithmeticCalculation();
+    } else if (calculationMode === VOWEL_COUNTING_MODE) {
+        performOneVowelCountingCalculation();
+    }
 }
 
 function printWelcomeMessage() {
     console.log('Welcome to the calculator! :D');
     console.log('This calculator can perform operations using the four operators: +, -, *, /.');
+}
+
+function getCalculationMode() {
+    return getInput(`Which calculator mode do you want?
+    1) Arithmetic
+    2) Vowel counting`)
 }
 
 function getInput(prompt) {
@@ -28,7 +41,7 @@ function getOperatorChoice() {
 }
 
 function getIterationValue() {
-    console.log('How many numbers do you wish to ' + operator + ' ?');
+    console.log(`How many numbers do you wish to ${operator}?`);
     return getNumberInput() ;
 }
 
@@ -36,7 +49,7 @@ function computeCalculation(operator, iterations) {
     let ans = 0;
 
     for (let i = 0; i < iterations; i++) {
-        console.log('Please enter number (' + (i+1) + ').')
+        console.log(`Please enter number ${i+1}.`)
         num = getNumberInput();
 
         if (i == 0){
@@ -59,11 +72,39 @@ function computeCalculation(operator, iterations) {
     return ans;
 }
 
-function performOneCalculation() {
+function performOneArithmeticCalculation() {
     operator = getOperatorChoice();
     iterations = getIterationValue();
-
     ans = computeCalculation(operator, iterations);
 
-    console.log('\nThe answer is ' + ans + '.');
+    console.log(`\nThe answer is ${ans}.`);
+}
+
+function getStringInput() {
+    return getInput('Please enter a string:').toUpperCase();
+}
+
+function countVowels(str) {
+    let vowelCounts = {A:0, E:0, I:0, O:0, U:0};
+    for (let char of str) {
+        if (char in vowelCounts) {
+            vowelCounts[char]++;
+        }
+    }
+    return vowelCounts;
+}
+
+function displayCounts(vowelCounts) {
+    console.log(`The vowel counts are:
+    A: ${vowelCounts.A}
+    E: ${vowelCounts.E}
+    I: ${vowelCounts.I}
+    O: ${vowelCounts.O}
+    U: ${vowelCounts.U}`);
+}
+
+function performOneVowelCountingCalculation() {
+    str = getStringInput();
+    vowelCounts = countVowels(str);
+    displayCounts(vowelCounts);
 }
